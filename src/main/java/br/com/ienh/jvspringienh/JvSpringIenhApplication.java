3a90a8d63895f1d95831de1cc9bb433e694bd32f
lluiziull <luizkirsch0@gmail.com>
2024-04-18 20:22:38 -0300

    first commit

---
+++ src/main/java/br/com/ienh/jvspringienh/JvSpringIenhApplication.java
@@ -0,0 +1,13 @@
+package br.com.ienh.jvspringienh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvSpringIenhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvSpringIenhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ienh/jvspringienh/JvSpringIenhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ienh.jvspringienh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvSpringIenhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
